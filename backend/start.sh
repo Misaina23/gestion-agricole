682a58a63fdbfd5fe5dfe4741fe576f8325d3448
Misaina23 <andrianisaina23@gmail.com>
2026-08-02 06:15:27 +0300

    Add demo data seeding to startup script

--- backend/start.sh
+++ backend/start.sh
@@ -8,6 +8,7 @@ python manage.py collectstatic --noinput
 
 echo "Loading initial data..."
 python manage.py load_initial_data
+python manage.py seed_demo_data
 
 echo "Starting Gunicorn..."
 gunicorn videeko_vanilla.wsgi:application --bind 0.0.0.0:8000
